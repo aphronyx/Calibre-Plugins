ee9cc7a0f1455a78f291cabfd6dd544658473d93
Aphronyx <aphronyx.su@gmail.com>
2024-11-25 18:27:47 +0800

    feat(main): search for books by keyword

--- readmoo/main.py
+++ readmoo/main.py
@@ -1,11 +1,16 @@
+from operator import itemgetter
+from threading import Thread
+from typing import Self
+
 import requests
+from lxml import html
 
 ID_NAME: str = "readmoo"
 
 
 class Book:
     def __init__(self, id: str) -> None:
-        url = f"https://readmoo.com/book/{id}"
+        url = f"{self.__DOMAIN}/book/{id}"
         res = requests.get(url)
         res_status = res.status_code
         if res_status == 404:
@@ -17,8 +22,37 @@ class Book:
                 f"Reason: {res_status} {res.reason}"
             )
 
+        self.__id = id
         self.__url = url
+
+    @classmethod
+    def search(cls, keyword: str) -> list[Self]:
+        url = f"{cls.__DOMAIN}/search/keyword?q={keyword}&page=1&st=true"
+        res = requests.get(url).text
+        ids = html.fromstring(res).xpath("//a[@class='product-link']/@data-readmoo-id")
+        max = ids_len if (ids_len := len(ids)) < 20 else 20
+
+        results = []
+        threads = []
+        for index, id in enumerate(ids[:max]):
+            thread = Thread(target=cls.__append_to, args=(index, id, results))
+            thread.start()
+            threads.append(thread)
+        [thread.join() for thread in threads]
+
+        books = [book for _, book in sorted(results, key=itemgetter(0))]
+        return books
+
+    @property
+    def id(self) -> str:
+        return self.__id
 
     @property
     def url(self) -> str:
         return self.__url
+
+    __DOMAIN: str = "https://readmoo.com"
+
+    @classmethod
+    def __append_to(cls, index: int, id: str, results: list[tuple[int, Self]]) -> None:
+        results.append((index, cls(id)))
--- tests/readmoo.py
+++ tests/readmoo.py
@@ -22,3 +22,44 @@ class TestBook(TestCase):
         id = "210305007000101"
 
         self.assertEqual(Book(id).url, f"https://readmoo.com/book/{id}")
+
+    def test_search_by_keyword(self) -> None:
+        expected = [
+            "290305214000101",
+            "210305007000101",
+            "210327431000101",
+            "210151993000101",
+            "210169924000101",
+            "210327452000101",
+            "210004400000101",
+            "210355874000101",
+            "210225629000101",
+            "210133438000101",
+            "210188083000101",
+            "210163830000101",
+            "210346931000101",
+            "210088159000101",
+            "210312649000101",
+            "210337462000101",
+            "220139496000101",
+            "210354142000101",
+            "210350346000101",
+            "210051998000101",
+        ]
+
+        self.assertEqual([book.id for book in Book.search("時代如何轉了彎")], expected)
+
+    def test_search_by_real_isbn(self) -> None:
+        books = Book.search("9786267229903")
+
+        self.assertEqual(len(books), 1)
+        self.assertEqual(books[0].id, "210305007000101")
+
+    def test_search_by_fake_isbn(self) -> None:
+        self.assertFalse(Book.search("9784531372638"))
+
+    def test_search_by_eisbn(self) -> None:
+        books = Book.search("9786267229897")
+
+        self.assertEqual(len(books), 1)
+        self.assertEqual(books[0].id, "210305007000101")
