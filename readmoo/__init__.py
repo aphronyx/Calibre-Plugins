378324c7438ff04798ec008bfbe7208db5b7bdfe
Aphronyx <aphronyx.su@gmail.com>
2024-11-25 18:33:08 +0800

    feat: get book url by eisbn

--- readmoo/__init__.py
+++ readmoo/__init__.py
@@ -8,9 +8,14 @@ class Readmoo(Source):
     author = "Aphronyx So͘"
     version = (0, 0, 0)
 
-    def get_book_url(self, identifiers):
+    def get_book_url(self, identifiers):  # pyright: ignore [reportIncompatibleMethodOverride]
         if id := identifiers.get(ID_NAME):
             return (ID_NAME, id, Book(id).url)
 
         if (isbn := identifiers.get("isbn")) and (books := Book.search(isbn)):
             return ("isbn", isbn, books[0].url)
+
+        if (eisbn := identifiers.get("eisbn")) and (books := Book.search(eisbn)):
+            return ("eisbn", eisbn, books[0].url)
+
+        return None
